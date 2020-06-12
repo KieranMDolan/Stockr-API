96d45753ff26e279098c26c4274fbbbd94089fd9
KieranMDolan <44838201+KieranMDolan@users.noreply.github.com>
2020-06-12 16:52:13 +1000

    Serve over self signed certificate on https

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const logger = require("morgan");
 const helmet = require('helmet');
 const cors = require('cors');
 
+const fs = require('fs');
+const https = require("https");
+const privateKey = fs.readFileSync('./sslcert/cert.key','utf8');
+const certificate = fs.readFileSync('./sslcert/cert.pem','utf8');
+const credentials = {
+ key: privateKey,
+ cert: certificate
+};
+
 const indexRouter = require("./routes/queryRouter");
 const usersRouter = require("./routes/usersRouter");
 const swaggerRouter = require("./routes/swaggerRouter");
@@ -15,7 +24,7 @@ const app = express();
 
 app.use(cors());
 app.use(helmet());
-app.use(logger("dev"));
+app.use(logger("common"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -42,5 +51,9 @@ logger.token("res", (req, res) => {
   res.getHeaderNames().map((h) => (headers[h] = res.getHeader(h)));
   return JSON.stringify(headers);
 });
+
+// create https server
+const server = https.createServer(credentials, app);
+server.listen(443);
 
 module.exports = app;
