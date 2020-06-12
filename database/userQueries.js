7ffce1b389d01a78f6ddfab001933d37a58febec
KieranMDolan <44838201+KieranMDolan@users.noreply.github.com>
2020-06-12 14:25:48 +1000

    Document userQueries

--- database/userQueries.js
+++ database/userQueries.js
@@ -1,10 +1,22 @@
 const bcrypt = require("bcrypt");
 
+// User/Auth Services related db queries
+
+/**
+ * get all users with the email passed through the request object
+ * @param {*} req
+ */
 const getUsersWithEmail = (req) => {
   const email = req.body.email;
   return req.db.from("users").select("*").where("email", email);
 };
 
+/**
+ * Inserts a new user into the users table with the email and a hashed + salted password
+ * @param {*} req
+ * @param {*} email
+ * @param {*} password
+ */
 const insertUser = (req, email, password) => {
   const saltRounds = 10;
   const hash = bcrypt.hashSync(password, saltRounds);
