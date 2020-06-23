de3bd2827e65b000bc6c5120f8284a4942005b5c
KieranMDolan <44838201+KieranMDolan@users.noreply.github.com>
2020-06-24 00:43:46 +1000

    Remove knex as express middleware

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const options = require("./knexfile.js");
-const knex = require("knex")(options);
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
@@ -29,10 +27,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use((req, res, next) => {
-  req.db = knex;
-  next();
-});
 
 // set up routers
 app.use("/stocks", indexRouter);
