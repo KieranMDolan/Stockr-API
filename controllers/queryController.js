fb6489fb085cc863a260b30f77eb64017815b71f
KieranMDolan <44838201+KieranMDolan@users.noreply.github.com>
2020-06-24 00:56:45 +1000

    Send responses in controller

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -1,8 +1,43 @@
-const { handleSymbolsRequest, handleUnauthedSymbolRequest, handleAuthedSymbolRequest } = require("../services/stockServices");
+const {
+  getAllSymbols,
+  getSymbolsByIndustry,
+  handleUnauthedSymbolRequest,
+  handleAuthedSymbolRequest,
+} = require("../services/stockServices");
 
-const getSymbols = (req, res, next) => {
-  handleSymbolsRequest(req, res, next);
-};
+async function getSymbols(req, res, next) {
+  // handleSymbolsRequest(req, res, next);
+  // let queryLength = Object.keys(req.query).length;
+  let queryExists = Object.keys(req.query).length > 0;
+  if (!queryExists) {
+    try {
+      getAllSymbols(req, res, next);
+    } catch (e) {
+      if (!e.status) {
+        res.status(500).json({ error: true, message: "Unknown Error" });
+      } else {
+        res.status(e.status).json({ error: true, message: e.message });
+      }
+    }
+  } else if (queryExists && req.query.industry) {
+    try {
+      const stocks = await getSymbolsByIndustry(req, res, next);
+      res.status(200).json(stocks);
+    } catch (e) {
+      console.log(e);
+      if (!e.status) {
+        res.status(500).json({ error: true, message: "Unknown Error" });
+      } else {
+        res.status(e.status).json({ error: true, message: e.message });
+      }
+    }
+  } else {
+    res.status(400).json({
+      error: true,
+      message: "Invalid query parameter: only 'industry' is permitted",
+    });
+  }
+}
 
 const getSingleSymbol = (req, res, next) => {
   handleUnauthedSymbolRequest(req, res, next);
