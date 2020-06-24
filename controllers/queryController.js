b788173043effc797f67ae8dd8f09f16429721d0
KieranMDolan <44838201+KieranMDolan@users.noreply.github.com>
2020-06-24 23:35:32 +1000

    Simplify symbols request controller

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -5,32 +5,40 @@ const {
   handleAuthedSymbolRequest,
 } = require("../services/stockServices");
 
-async function getSymbols(req, res, next) {
+async function handleUnqueriedSymbolRequest(res) {
+  try {
+    const stocks = await getAllSymbols();
+    res.status(200).json(stocks);
+  } catch (e) {
+    if (!e.status) {
+      res.status(500).json({ error: true, message: "Unknown Error" });
+    } else {
+      res.status(e.status).json({ error: true, message: e.message });
+    }
+  }
+}
+
+async function handleQueriedSymbolRequest(req, res, next) {
+  try {
+    const stocks = await getSymbolsByIndustry(req, res, next);
+    res.status(200).json(stocks);
+  } catch (e) {
+    console.log(e);
+    if (!e.status) {
+      res.status(500).json({ error: true, message: "Unknown Error" });
+    } else {
+      res.status(e.status).json({ error: true, message: e.message });
+    }
+  }
+}
+
+function getSymbols(req, res, next) {
   let queryExists = Object.keys(req.query).length > 0;
-  
+
   if (!queryExists) {
-    try {
-      const stocks = await getAllSymbols();
-      res.status(200).json(stocks);
-    } catch (e) {
-      if (!e.status) {
-        res.status(500).json({ error: true, message: "Unknown Error" });
-      } else {
-        res.status(e.status).json({ error: true, message: e.message });
-      }
-    }
+    handleUnqueriedSymbolRequest(res);
   } else if (queryExists && req.query.industry) {
-    try {
-      const stocks = await getSymbolsByIndustry(req, res, next);
-      res.status(200).json(stocks);
-    } catch (e) {
-      console.log(e);
-      if (!e.status) {
-        res.status(500).json({ error: true, message: "Unknown Error" });
-      } else {
-        res.status(e.status).json({ error: true, message: e.message });
-      }
-    }
+    handleQueriedSymbolRequest(req, res, next);
   } else {
     res.status(400).json({
       error: true,
